811ebdce25d903e37fe18ba07f31d57b9e5f51b6
Igor Zhukov <igorek993@gmail.com>
2020-05-22 18:11:23 +1000

    3.1 start writing the check fun

--- FooBar/3.1.py
+++ FooBar/3.1.py
@@ -4,15 +4,23 @@ def solution(x, y):
     if x == ('1' or '2') or y == ('1' or '2'):
         return '1'
     current_list = [['1', '2']]
+    while True:
+        new_list = get_next_list(current_list)
+        # TODO check fun here
+
+        current_list = new_list
+        new_list = list()
+        print(current_list)
+
+
+def get_next_list(current_list):
     new_list = list()
     for _list in current_list:
         position = 1
         for digit in _list:
             new_list.append((str(int(digit) + int(_list[position])), str(int(_list[position]))))
             position = position - 1
-    current_list = new_list
-    new_list = list()
-    print(current_list)
+    return new_list
 
 
 print(solution('4', '7'))
